358674ba4ac2a0251421a9e5e27a149d1b2b4bb4
periridwann <periridwan35@gmail.com>
2023-03-03 15:37:13 +0700

    commit

--- app.py
+++ app.py
@@ -1,30 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
+import pandas as pd
 import time
 import mysql.connector
 
-# agar tidak menampilkan browser saat menjalankan script
 opsi = webdriver.ChromeOptions()
-# untuk 
-opsi.add_argument('--headless')
+opsi.add_argument('--headless') # agar tidak menampilkan browser saat menjalankan script
 servis = Service('chromedriver.exe')
 driver = webdriver.Chrome(service=servis, options=opsi)
 
 shopee_link = 'https://shopee.co.id/search?keyword=tas%20pria'
-driver.set_window_size(1300,800)
+driver.set_window_size(1300,800) # set window size agar semua data dapat diakses
 driver.get(shopee_link)
 
-# berapa panjang scroll kebawah (500 pixel)
-rentang = 500
-# dibuat sampai 7 agar (1 sampai 6)
-for i in range(1,7):
+rentang = 500 # panjang scroll kebawah (500 pixel)
+
+for i in range(1,7): # dibuat scroll sebanyak 6 kali agar mendapatkan semua data
     akhir = rentang * i
-# buat perintah menggunakan javascript
-    perintah = "window.scrollTo(0,"+str(akhir)+")"
+    perintah = "window.scrollTo(0,"+str(akhir)+")" # buat perintah menggunakan javascript
     driver.execute_script(perintah)
-    # untuk menampilkan sudah berapa halaman yang discroll
-    print("loading ke-"+str(i))
+    print("loading ke-"+str(i)) # untuk menampilkan sudah berapa halaman yang discroll
     time.sleep(1)
 
 time.sleep(10)
@@ -32,15 +28,11 @@ time.sleep(10)
 content = driver.page_source
 driver.quit()
 
-# beautifulsoup untuk parsing data
-data = BeautifulSoup(content, 'html.parser')
-# print(data.encode('utf-8'))
-
+# ----------------- SCRAPING DATA --------
 i = 1
+data = BeautifulSoup(content, 'html.parser') # beautifulsoup untuk parsing data
 base_url = 'https://shopee.co.id'
-
-list_nama_produk, list_harga_produk, list_lokasi_penjualan, list_produk_terjual, list_link_penjualan=[],[],[],[],[]
-
+list_produk=[]
 for area in data.find_all('div',class_="col-xs-2-4 shopee-search-item-result__item"):
     print('proses data ke-'+str(i))
     nama_produk = area.find('div',class_="ie3A+n bM+7UW Cve6sh").get_text()
@@ -51,15 +43,13 @@ for area in data.find_all('div',class_="col-xs-2-4 shopee-search-item-result__it
         produk_terjual = produk_terjual.get_text()
     link_penjualan = base_url + area.find('a')['href']
 
-    list_nama_produk.append(nama_produk)
-    list_harga_produk.append(harga_produk)
-    list_lokasi_penjualan.append(lokasi_penjualan)
-    list_produk_terjual.append(produk_terjual)
-    list_link_penjualan.append(link_penjualan)
+    list_produk.append(
+        (nama_produk, harga_produk, lokasi_penjualan, produk_terjual, link_penjualan)
+    )
     i+=1
     print("------")
     
-    # save to mysql
+    # ----------------- SAVE TO MYSQL --------
     cnx = mysql.connector.connect(user='root', database='shopee')
     cursor = cnx.cursor()
 
@@ -68,13 +58,8 @@ for area in data.find_all('div',class_="col-xs-2-4 shopee-search-item-result__it
                 "VALUES (%s, %s, %s, %s, %s)")
 
     data_product = (nama_produk, harga_produk, lokasi_penjualan, produk_terjual, link_penjualan)
-
-    # Insert new employee
     cursor.execute(add_product, data_product)
     emp_no = cursor.lastrowid
-
-    # Make sure data is committed to the database
-    cnx.commit()
-
+    cnx.commit() # commit ke database
     cursor.close()
     cnx.close()
